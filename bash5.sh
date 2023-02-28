d940d8dcc3d1315b5d2f90090dfe7de68dfd7730
atharva <atharvaatak22@gmail.com>
2023-02-28 18:59:02 +0530

    fix

--- bash5.sh
+++ bash5.sh
@@ -1,1 +1,1 @@
-cat "*/01 * * * * root bash /var/lib/jenkins/workspace/flask_deploy_master/bash4.sh" > /etc/cron.d/mycron
+sudo -i cat "*/01 * * * * root bash /var/lib/jenkins/workspace/flask_deploy_master/bash4.sh" > /etc/cron.d/mycron
